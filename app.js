b990e7d47ec0af42d5bf93786b38108b16fda6fb
muddassar <mslearn0055@gmail.com>
2024-11-19 20:17:55 +0530

    new app file

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>zeeshan bhai udgir waale ,khateeb saab ke dost , kon khateeb saab ? bulle baal khateib ... siraj saab , samsun, maniyar kille galli ke damad </p>');
+})
+
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
+
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
+ 
